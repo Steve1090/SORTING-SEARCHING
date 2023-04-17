fcb8dd2780efa53c484c88df39ea0ac7e6f4711c
Steve <108262513+Steve1090@users.noreply.github.com>
2023-04-17 18:14:06 +0530

    Add files via upload

---
+++ SS-ALGORITHM.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int arr[5] = {10, 22, 66, 77, 82};
+    int sv;
+        	 printf("Enter the value of sv:");
+			scanf("%d",&sv);
+    for (int i = 0; i < 5; i++) {
+        if (arr[i] == sv) {
+        printf("Number found");
+          return 0; 
+        }
+    }
+
+    printf("Number not found");
+
+    return 0;
+}
+
+
+
+
+
+
