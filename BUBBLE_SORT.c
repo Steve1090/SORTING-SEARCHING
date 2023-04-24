7ddfb22922669b791e3dd44f631bc9d8bb647683
Steve <108262513+Steve1090@users.noreply.github.com>
2023-04-24 18:51:40 +0530

    Update BUBBLE_SORT.c

--- BUBBLE_SORT.c
+++ BUBBLE_SORT.c
@@ -1,43 +1,34 @@
-#include<stdio.h>
-void Bubble(int arr[],int n);
-void disp(int []);
-int main()
-{
-int arr[5]= {22,56,99,101,122};
-int n; 
-Bubble(arr, n);
+#include <stdio.h>
 
-//printf("Enter n:");
-//scanf("%d",&n);
-disp(arr);
+void Bubble(int arr[], int n);
+void disp(int arr[]);
 
+int main() {
+    int arr[5] = {22, 56, 101, 99, 122};
+    Bubble(arr, 5);
+    disp(arr);
+    return 0;
+}
 
+void Bubble(int arr[], int n) {
+    int passes, comp, i, j;
+    int temp;
+    passes = 4; 
+    comp = 4;
+    for(i = 0; i < passes; i++) {
+        for(j = 0; j < comp - i; j++) {
+            if(arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
 }
-void Bubble(int arr[],int n)
-{
-int passes, comp, i, j;
-int temp;
-passes= n-1; 
-comp=n-1; 
-for(i=0;i<passes; i++)
-{
-	for(j=0; j<comp-i;j++)
-	{
-		if(arr[j]>arr[j+1])
-		{
-			temp=arr[j];
-			arr[j]=arr[j+1];
-			arr[j+1]=temp;
-		}
-	}
-}
-}
-void disp(int arr[])
-{
-int i;
-for(i=0;i<5;i++)
-{
 
-	printf("The value is %d:", arr[i]);
-}
+void disp(int arr[]) {
+    int i;
+    for(i = 0; i < 5; i++) {
+        printf("The value is %d\n", arr[i]);
+    }
 }
